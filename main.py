c15a1da564634ce8b7e59bcffb1b4c31e23928b8
Ever <78941506+edfp19@users.noreply.github.com>
2025-03-03 18:50:32 +0000

    First project in boot.dev over!

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from stats import num_words, character_count
-relative_path= "books/frankenstein.txt"
+from stats import num_words, character_count, sort_dict
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    pass
+
+relative_path= sys.argv[1]
+sorted_dict = sort_dict(relative_path)
+print(f"============ BOOKBOT ============\n Analyzing book found at {relative_path}...")
+print("----------- Word Count ----------")
 num_words(relative_path)
-print(character_count(relative_path))
+print("--------- Character Count -------")
+for dictionary in sorted_dict:
+    print(f"{dictionary["key"]}: {dictionary["num"]}")
--- stats.py
+++ stats.py
@@ -1,20 +1,38 @@
+# this is to open the .txt and read its contents 
 def get_book_text(filepath) -> str:
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+
+#this is to count the numbers of words
 def num_words(relative_path):
     file_contents = get_book_text(relative_path)
     text = file_contents.split()
     num_words = len(text)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
+# this is to count the number of characters
 def character_count(relative_path):
     file_contents = get_book_text(relative_path)
     array_of_characters = file_contents.lower()
     dictionary = {}
     for char in array_of_characters:
-            if char in dictionary: 
+            if char in dictionary and char.isalpha(): 
                 dictionary[char] += 1
             else:
                 dictionary[char] = 1
     return dictionary
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dict(relative_path):
+    # first we want to read the data 
+    text = character_count(relative_path)
+    list_of_dicts = []
+    for key, value in text.items():
+        list_of_dicts.append({"key":key, "num":value})
+        list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+    print(list_of_dicts)
+
